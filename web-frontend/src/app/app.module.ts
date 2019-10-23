57f4295d596545c27c9d21987da8ec111e26a476
rodchenk <rodchenk@th-brandenburg.de>
2019-10-23 13:51:31 +0200

    renamed folders

--- web-frontend/src/app/app.module.ts
+++ web-frontend/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
