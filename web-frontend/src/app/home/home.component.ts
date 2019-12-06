55ebad8ffa71155e76ef740b5658982f7b3bdebb
rodchenk <rodchenk@th-brandenburg.de>
2019-12-06 14:11:40 +0100

    made sidepanel invisible, create news interface (?)

--- web-frontend/src/app/home/home.component.ts
+++ web-frontend/src/app/home/home.component.ts
@@ -5,11 +5,31 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+	public cards:any;
+  	constructor() { }
 
-  ngOnInit() {
-  }
+  	ngOnInit() {
+  		this.cards = [
+  			new News('foliage Studio','Mischa'),
+  			new News('Developing new features', 'Ksusha')
+  		];
+  	}
 
+}
+interface NewsInterface{
+	title:string;
+	author:string;
+}
+class News implements NewsInterface{
+	
+	title:string;
+	author:string;
+
+	constructor(title:string = '', author:string = ''){
+		this.title = title;
+		this.author = author;
+	}
 }
