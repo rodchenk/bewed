14c2b9f113cfa838b7bfc861a8bddead33f5be4b
rodchenk <rodchenk@th-brandenburg.de>
2019-12-07 12:47:31 +0100

    sign up component

--- web-frontend/src/app/register/register.component.ts
+++ web-frontend/src/app/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
+import { UserService } from './../user.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  	private registerForm:any
+	public hide:boolean = true
+	private host:string = 'http://127.0.0.1:3000'
+	private service:string = '/auth'
 
-  ngOnInit() {
-  }
+  	constructor(private formBuilder: FormBuilder, private http: HttpClient, private toast: ToastrService, private userProvider: UserService) {
+  		this.registerForm = new FormGroup({
+			name: new FormControl(),
+			password: new FormControl(),
+			email: new FormControl()
+		});
+  	}
 
+  	onSubmit(values:any){
+  		if(!values.email || !values.name || !values.password) return;
+  		console.log('register')
+  	}
+
+  	ngOnInit() {}
 }
