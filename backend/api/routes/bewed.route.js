4416649e23000785c45a32e2178e742ef0603049
rodchenk <rodchenk@th-brandenburg.de>
2019-12-14 01:10:02 +0100

    add API put method to user service

--- backend/api/routes/bewed.route.js
+++ backend/api/routes/bewed.route.js
@@ -3,15 +3,20 @@
 var bewed = 	require('../controllers/bewed.controller');
 var projects = 	require('../controllers/projects.controller');
 var pool = 		require('../controllers/pool.controller');
+var user =		require('../controllers/user.controller');
 
 const express = require('express')
 const router = express.Router()
 
+/* TODO remove this api calls */
 router.get('/test', bewed.test)
-router.get('/user', bewed.getById)
-router.post('/user', bewed.add_user)
 router.get('/projects', projects.getAll)
 
+/* user api */
+router.get('/user', user.getByID)
+router.put('/user', user.update)
+
+/* pool api */
 router.post('/pool', pool.add)
 router.get('/pool/all', pool.getAll)
 router.get('/pool', pool.getByUser)
