88747a5c3a7e8b7add6bda91947eb3ebfa1d548a
rodchenk <rodchenk@th-brandenburg.de>
2020-01-19 21:59:27 +0100

    add check if file is already encrypted or decrypted

--- docs/char_encrypter.py
+++ docs/char_encrypter.py
@@ -3,11 +3,12 @@ class CharEncrypter(object):
 	def __init__(self, file_name, _key_file):
 		self.file_name = file_name
 		self.__SPACE = '0xS'
+		self.__ENCRYPT_CERT = '[encrypted with char_encrypter]'
 		try:
 			with open(_key_file) as key_object:
 				self._key = int(key_object.read())
 		except Exception as e:
-			print('Key not found')
+			print(e)
 
 	def _write(self):
 		pass
@@ -21,7 +22,13 @@ class CharEncrypter(object):
 		temp_content = []
 		try:
 			with open(self.file_name, 'r') as file_object:
-				for line in file_object.read().split('\n'):
+				if self.__ENCRYPT_CERT in file_object.readline():
+					content = file_object.read().split('\n')
+				else:
+					print('File is not encrypted')
+					raise 
+						
+				for line in content:
 					if len(line) == 0:
 						continue
 					temp_line = ''
@@ -30,11 +37,11 @@ class CharEncrypter(object):
 							temp_line += ' '
 						else:
 							char_num = int( int(word, 16) / self._key)
-
 							temp_line += str(chr(char_num))
+
 					temp_content.append(temp_line + '\n')
 					if self.log_on:
-						print('Line decrypted')
+						print('\t- Line decrypted')
 			with open(self.file_name, 'w') as file_object:
 				# remove last newLine character
 				temp_content[-1] = temp_content[-1].replace('\n', '')
@@ -50,7 +57,12 @@ class CharEncrypter(object):
 		temp_content = []
 		try:
 			with open(self.file_name, 'r') as file_object:
-				for line in file_object.read().split('\n'):
+				content = file_object.read().split('\n')
+				if self.__ENCRYPT_CERT in content[0]:
+					print('File is already encrypted')
+					raise
+
+				for line in content:
 					if len(line) == 0:
 						continue
 					for word in line.split():
@@ -60,12 +72,13 @@ class CharEncrypter(object):
 							temp += str(hex(char_num * self._key)) + ' '
 						temp_content.append(temp + self.__SPACE + ' ')
 					if self.log_on:
-						print('Line encrypted')
+						print('\t- Line encrypted')
 					temp_content.append('\n')
 
 			with open(self.file_name, 'w') as file_object:
 				# remove last newLine character
 				temp_content.pop()
+				file_object.write(self.__ENCRYPT_CERT + '\n')
 				file_object.write(''.join(temp_content))
 				if self.log_on:
 					print('Encryption finished')
